23d7d32142e48ada577ab8deca9f97beceab7111
Zach Caldwell <zach.caldwell101@gmail.com>
2019-03-18 14:39:50 -0600

    [Xelandria] Syntactic tweaks for code cleanliness

--- code/gameMain.js
+++ code/gameMain.js
@@ -1,22 +1,40 @@
+"use strict";
 
 xel.gameLoad = function() {
    xel.hx.loadingBar();
 }
 
+
 xel.gameStateStart = function() {
    
 }
+
 
 xel.gameSetup = function() {
    xel.hx.backgroundColor = 0x10a010;
    xel.hx.state = xel.gameStateStart;
 }
 
+
 xel.listPreloadAssets = function() {
    
 }
 
-xel.gameMain = function() {
+
+function main() {
+    // Sanity check: make sure container object and libraries are already defined
+    if ((typeof xel == 'undefined')
+     || (typeof hexi == 'undefined')
+     || (typeof EntityManager == 'undefined')) {
+        var errorMsg = "gameMain ERROR: Main object or library not loaded; aborting.";
+        console.error(errorMsg);
+        console.log("xel: " + (typeof xel));
+        console.log("hexi: " + (typeof hexi));
+        console.log("EntityManager: " + (typeof EntityManager));
+        document.getElementsByTagName('body')[0].innerHTML += "<h4>" + errorMsg + "</h4>";
+        return;
+    }
+
    // Set up canvas size and FPS
    // For now, just use window size and default fps
    xel.windowWidth = window.innerWidth;
