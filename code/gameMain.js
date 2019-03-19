d4eb82ce00d2b8ccd2a1cbe8106c8184c8e7cd41
Zach Caldwell <zach.caldwell101@gmail.com>
2019-03-19 00:23:47 -0600

    [xelandria] Fix scene system and add some test sprites

--- code/gameMain.js
+++ code/gameMain.js
@@ -29,6 +29,21 @@ xel.gameSetup = function () {
 
   // Add event handlers to call hexi pause and resume
 
+  // Add some sample stuff
+  xel.sc1 = xel.mgr.getScene("sc1");
+  xel.spr1 = xel.hx.sprite("assets/placeable/library/bookcaseBooks_SW.png");
+  xel.spr2 = xel.hx.sprite("assets/placeable/library/bookcaseGlass_S.png");
+  xel.spr1.vx = 5 * xel.hx.randomInt(-1, -0.1);
+  xel.spr1.vy = 5 * xel.hx.randomInt(0.1, 1);
+  xel.spr2.vx = 5 * xel.hx.randomInt(0.1, 1);
+  xel.spr2.vy = 5 * xel.hx.randomInt(-1, -0.1);
+  xel.spr1.scale = xel.spr2.scale = {
+    x: 0.5,
+    y: 0.5
+  };
+  xel.sc1.add(xel.spr1, xel.spr2);
+  xel.mgr.switchToScene(xel.sc1);
+
   // Set the current state-loop (game-loop) function
   xel.hx.state = xel.gameStateStart;
 };
@@ -36,13 +51,6 @@ xel.gameSetup = function () {
 
 // Set up Hexi object; platform variables and assetList[] should already be set up 
 xel.hx = hexi(xel.windowWidth, xel.windowHeight, xel.gameSetup, xel.assetList, xel.gamePreload);
-
-
-// Scenes are an extension of Hexi groups
-xel.Scene = function (name) {
-  this.name = name;
-};
-xel.Scene.prototype = xel.hx.group();
 
 
 // Scene manager (separate from Hexi's scene graph)
@@ -54,7 +62,8 @@ xel.SceneManager.prototype = {
     if (name in this.scenes) {
       return this.scenes[name];
     } else {
-      var sc = new xel.Scene(name);
+      var sc = xel.hx.group();
+      sc.name = name;
       this.scenes[name] = sc;
       this.hideScene(sc);
       return sc;
@@ -99,7 +108,28 @@ xel.SceneManager.prototype = {
 
 
 xel.gameStateStart = function() {
-  
+  xel.hx.move([xel.spr1, xel.spr2]);
+
+  var cnt = xel.hx.contain(xel.spr1, 
+  {
+    x: 1, y: 1,
+    width: xel.hx.canvas.width - 2,
+    height: xel.hx.canvas.height - 2
+  });
+  if (cnt) {
+    if (cnt.has("top") || cnt.has("bottom"))
+      xel.spr1.vy *= -1;
+    else if (cnt.has("left") || cnt.has("right"))
+      xel.spr1.vx *= -1;
+  }
+
+  cnt = xel.hx.contain(xel.spr2, xel.hx.stage);
+  if (cnt) {
+    if (cnt.has("top") || cnt.has("bottom"))
+      xel.spr2.vy *= -1;
+    else if (cnt.has("left") || cnt.has("right"))
+      xel.spr2.vx *= -1;
+  }
 };
 
 
