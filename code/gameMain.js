53eb5ade5ed12f15cf316eeb946e764502dd11f0
Zach Caldwell <zach.caldwell101@gmail.com>
2019-03-18 21:10:03 -0600

    [Xelandria] Add Scene and SceneManager

--- code/gameMain.js
+++ code/gameMain.js
@@ -1,4 +1,65 @@
 "use strict";
+if (typeof xel == 'undefined') {
+  xel = {};
+}
+
+
+xel.Scene = function(name) {
+  this.name = name;
+}
+xel.Scene.prototype = new hexi.group();
+
+
+xel.SceneManager = function() {
+  this.scenes = [];
+}
+xel.SceneManager.prototype = {
+  getScene: function(name) {
+    if (name in scenes) {
+      return scenes[name];
+    }
+    else {
+      var sc = new Scene(name);
+      scenes[name] = sc;
+      hideScene(sc);
+      return sc;
+    }
+  },
+
+  hideScene: function(scene) {
+    if (typeof scene === 'undefined') {
+      console.error("ERROR hideScene(): No scene given.");
+      return;
+    }
+    if (typeof scene === 'string') {
+      getScene(scene).visible = false;
+    }
+    else {
+      scene.visible = false;
+    }
+  },
+
+  showScene: function(scene) {
+    if (typeof scene === 'undefined') {
+      console.error("ERROR showScene(): No scene given.");
+      return;
+    }
+    if (typeof scene === 'string') {
+      getScene(scene).visible = true;
+    }
+    else {
+      scene.visible = true;
+    }
+  },
+
+  switchToScene: function(scene) {
+    for (s in scenes) {
+      hideScene(s);
+    }
+    showScene(scene);
+  }
+}
+
 
 xel.gameLoad = function() {
   xel.hx.loadingBar();
@@ -11,7 +72,12 @@ xel.gameStateStart = function() {
 
 
 xel.gameSetup = function() {
-  xel.hx.backgroundColor = 0x10a010;
+  // Load splash screen
+
+  // Start delta timer
+
+  // Add event handlers to call hexi pause and resume
+
   xel.hx.state = xel.gameStateStart;
 }
 
@@ -22,13 +88,11 @@ xel.listPreloadAssets = function() {
 
 
 function main() {
-  // Sanity check: make sure container object and libraries are already defined
-  if ((typeof xel == 'undefined')
-   || (typeof hexi == 'undefined')
+  // Sanity check: make sure libraries are already defined
+  if ((typeof hexi == 'undefined')
    || (typeof EntityManager == 'undefined')) {
-      var errorMsg = "gameMain ERROR: Main object or library not loaded; aborting.";
+      var errorMsg = "ERROR main(): Required library object(s) not found; aborting.";
       console.error(errorMsg);
-      console.log("xel: " + (typeof xel));
       console.log("hexi: " + (typeof hexi));
       console.log("EntityManager: " + (typeof EntityManager));
       document.getElementsByTagName('body')[0].innerHTML += "<h4>" + errorMsg + "</h4>";
@@ -41,9 +105,15 @@ function main() {
   xel.windowHeight = window.innerHeight;
   xel.fps = 60;
 
-  // Launch Hexi
+  // Set up Hexi
   xel.hx = hexi(xel.windowWidth, xel.windowHeight, xel.gameSetup, xel.listPreloadAssets(), xel.gameLoad);
   xel.hx.fps = xel.fps;
+  xel.hx.backgroundColor = 0x666666;
   xel.hx.scaleToWindow();
+
+  // Set up SceneManager
+  xel.mgr = new xel.SceneManager();
+
+  // Launch the game loop
   xel.hx.start();
 }
