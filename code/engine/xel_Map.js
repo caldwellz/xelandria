c3de446f8aa3a3bedc15c244395231cdbb3be86e
Zach Caldwell <zach.caldwell101@gmail.com>
2019-06-27 20:47:21 -0600

    Create/return a new object in map constructor

--- code/engine/xel_Map.js
+++ code/engine/xel_Map.js
@@ -8,21 +8,22 @@ if (typeof PIXI === 'undefined') { throw "xel_Map.js: PIXI not loaded!"; }
 // ***
 
 xel.Map = function (tiledData) {
+  var obj = {};
   if (!tiledData.width || !tiledData.height || !tiledData.tilewidth || !tiledData.tileheight || !tiledData.orientation) {
     logger.error("Invalid map parameter(s)");
     logger.debug(tiledData);
     return null;
   }
 
-  this.width = tiledData.width;
-  this.height = tiledData.height;
-  this.tileWidth = tiledData.tilewidth;
-  this.tileHeight = tiledData.tileheight;
-  this.pixelWidth = this.width * this.tileWidth;
-  this.pixelHeight = this.height * this.tileHeight;
-  this.orientation = tiledData.orientation;
-  this.layers = new PIXI.Container();
-  this._spriteTiles = [];
+  obj.width = tiledData.width;
+  obj.height = tiledData.height;
+  obj.tileWidth = tiledData.tilewidth;
+  obj.tileHeight = tiledData.tileheight;
+  obj.pixelWidth = obj.width * obj.tileWidth;
+  obj.pixelHeight = obj.height * obj.tileHeight;
+  obj.orientation = tiledData.orientation;
+  obj.layers = new PIXI.Container();
+  obj._spriteTiles = [];
 
   var z = 1;
   for (var l in tiledData.layers) {
@@ -38,25 +39,24 @@ xel.Map = function (tiledData) {
         if (gid > 0) {
           var spr = new PIXI.Sprite();
           spr.zIndex = z;
-          spr.x = (i * this.tileWidth) % this.pixelWidth;
-          spr.y = ((i - (i % this.width)) / this.width) * this.tileHeight;
-          if (spr.y > this.pixelHeight)
-            logger.warn("Sprite position exceeds height of map '" + this.name + "'");
-          if (!this._spriteTiles[gid])
-            this._spriteTiles[gid] = [];
-          this._spriteTiles[gid].push(spr);
+          spr.x = (i * obj.tileWidth) % obj.pixelWidth;
+          spr.y = ((i - (i % obj.width)) / obj.width) * obj.tileHeight;
+          if (spr.y > obj.pixelHeight)
+            logger.warn("Sprite position exceeds height of map '" + obj.name + "'");
+          if (!obj._spriteTiles[gid])
+            obj._spriteTiles[gid] = [];
+          obj._spriteTiles[gid].push(spr);
           layer.addChild(spr);
         }
       }
     }
     layer.sortChildren();
-    this.layers.addChild(layer);
+    obj.layers.addChild(layer);
     ++z;
   }
 
   var a = document.createElement('a');
   var ld = new PIXI.Loader();
-  var ctx = this;
   for (var t in tiledData.tilesets) {
     // Convert to absolute URL for use as a normalized name
     a.href = tiledData.tilesets[t].source;
@@ -70,10 +70,10 @@ xel.Map = function (tiledData) {
         var sheet = resources[a.href].spritesheet;
         // TODO: Un-hardcode if we ever have other than 8 images per spritesheet
         for (var gid = firstgid; gid < (firstgid + 8); ++gid) {
-          if (ctx._spriteTiles[gid]) {
-            for (var n = 0; n < ctx._spriteTiles[gid].length; ++n) {
+          if (obj._spriteTiles[gid]) {
+            for (var n = 0; n < obj._spriteTiles[gid].length; ++n) {
               var texName = sheet._frameKeys[gid - firstgid];
-              ctx._spriteTiles[gid][n].texture = sheet.textures[texName];
+              obj._spriteTiles[gid][n].texture = sheet.textures[texName];
             }
           }
         }
@@ -84,5 +84,5 @@ xel.Map = function (tiledData) {
     }
   });
 
-  return this;
+  return obj;
 };
