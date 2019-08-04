7d5b23544b603c756a2ec6811f008edeb4aa93dc
Zach Caldwell <zach.caldwell101@gmail.com>
2019-08-04 16:00:40 -0600

    Remove hackish custom test system

--- code/logger.js
+++ code/logger.js
@@ -10,8 +10,6 @@ logger.logbox = document.createElement("div");
 logger.logbox.id = "logbox";
 document.body.appendChild(logger.logbox);
 
-logger.testsPassed = 0;
-logger.testsFailed = 0;
 
 logger.clear = function () {
   logger.messages = new Array();
@@ -38,7 +36,7 @@ logger.rewrite = function (msg) {
 
 
 logger.log = function (msg) {
-  if (msg && !logger.testMode) {
+  if (msg) {
     if (typeof msg === 'string')
       logger.push(msg);
     console.log(msg);
@@ -47,7 +45,7 @@ logger.log = function (msg) {
 
 
 logger.debug = function (msg) {
-  if (msg && logger.debugMode && !logger.testMode) {
+  if (msg && logger.debugMode) {
     if (typeof msg === 'string') {
       var msgHTML = '<span style="color: #909090;">[DBG] ' + msg + '</span>';
       logger.push(msgHTML);
@@ -58,7 +56,7 @@ logger.debug = function (msg) {
 
 
 logger.warn = function (msg) {
-  if (msg && !logger.testMode) {
+  if (msg) {
     if (typeof msg === 'string') {
       var msgHTML = '<span style="color: orange;">[WARN] ' + msg + '</span>';
       logger.push(msgHTML);
@@ -69,7 +67,7 @@ logger.warn = function (msg) {
 
 
 logger.error = function (msg) {
-  if (msg && !logger.testMode) {
+  if (msg) {
     if (typeof msg === 'string') {
       var msgHTML = '<span style="color: red;">[ERR] ' + msg + '</span>';
       logger.push(msgHTML);
@@ -77,67 +75,6 @@ logger.error = function (msg) {
     console.error(msg);
   }
 };
-
-
-logger.pass = function (msg) {
-  logger.testsPassed++;
-  if (msg && logger.testMode) {
-    if (typeof msg === 'string') {
-      var msgHTML = '<span style="color: green;">[PASSED] ' + msg + '</span>';
-      logger.push(msgHTML);
-    }
-    console.log(msg);
-  }
-};
-
-
-logger.fail = function (msg) {
-  logger.testsFailed++;
-  if (msg && logger.testMode) {
-    if (typeof msg === 'string') {
-      var msgHTML = '<span style="color: red;">[FAILED] ' + msg + '</span>';
-      logger.push(msgHTML);
-    }
-    console.warn(msg);
-  }
-};
-
-
-logger.assert = function (condition, msg) {
-  if (condition)
-    logger.pass(msg);
-  else
-    logger.fail(msg);
-}
-
-
-logger.beginTest = function (testName) {
-  if ((typeof testName === 'string') && logger.testMode) {
-    var msg = "*** Beginning test 'tests/" + testName + "' ***";
-    var msgHTML = '<span style="color: blue;">' + msg + '</span>';
-    logger.push(msgHTML);
-    console.log(msg);
-  }
-}
-
-
-logger.logTestStats = function () {
-  var msg = "Tests passed: " + String(logger.testsPassed);
-  logger.push('<span style="color: green;">' + msg + '</span>');
-  console.log(msg)
-  var msg = "Tests failed: " + String(logger.testsFailed);
-  logger.push('<span style="color: red;">' + msg + '</span>');
-  console.log(msg)
-  var msg = "Total tests: " + String(logger.testsFailed + logger.testsPassed);
-  logger.push('<span style="font-weight: bold;">' + msg + '</span>');
-  console.log(msg)
-};
-
-
-logger.clearTestStats = function () {
-  logger.testsFailed = 0;
-  logger.testsPassed = 0;
-}
 
 
 // Uncaught throws will typically be logged in console by the window
@@ -148,7 +85,6 @@ logger.boxError = function (msg) {
   }
 };
 window.onerror = logger.boxError;
-
 
 //RequireJS export
 if (requirejs)
